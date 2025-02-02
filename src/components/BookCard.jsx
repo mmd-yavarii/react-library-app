01674e1598ffc97b4cf83e232f559914868943c2
mmd-yavarii <mdyavarii@gmail.com>
2025-02-02 18:09:17 +0330

    handle like button and style book cards

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import { AiFillHeart } from 'react-icons/ai';
+
+import styles from './BookCard.module.css';
+
 function BookCard({ data: { title, image, author, language, pages } }) {
+    const [like, setLike] = useState(false);
+
+    function likeHandler() {
+        setLike((pre) => !pre);
+    }
+
     return (
-        <div>
+        <div className={styles.card}>
             <img src={image} alt={title} />
-            <div>
+            <div className={styles.info}>
                 <h3>{title}</h3>
                 <p>{author}</p>
                 <div>
@@ -10,7 +21,12 @@ function BookCard({ data: { title, image, author, language, pages } }) {
                     <span>{pages}</span>
                 </div>
             </div>
-            <button>like</button>
+            <button onClick={likeHandler}>
+                <AiFillHeart
+                    color={like ? 'red' : '#e0e0e0'}
+                    fontSize="1.5rem"
+                />
+            </button>
         </div>
     );
 }
